7b6ee9743422186b1ddaa39a1ddba25f151abd1b
abdula277 <gripanov@gmail.com>
2017-11-01 09:00:05 +0300

    Make refactoring

--- FibHeap/FibHeap.cs
+++ FibHeap/FibHeap.cs
@@ -6,170 +6,162 @@ using System.Threading.Tasks;
 
 namespace FibHeap
 {
-  class FibHeap
-  {
-    private Node myMinNode = null; // узел с минимальным корнем
-    private int mySize = 0; // количество узлов
-
-    public void Push(int key)
+    class FibHeap
     {
-      var newNode = new Node(key);
-      if (mySize == 0)
-      {
-        myMinNode = newNode;
-        myMinNode.left = newNode;
-        myMinNode.right = newNode;
-      }
-      else
-      {
-        var prevRightNode = myMinNode.right;
-        myMinNode.right = newNode;
-        newNode.left = myMinNode;
-        newNode.right = prevRightNode;
-        prevRightNode.left = newNode;
-      }
-      if (newNode.myKey < myMinNode.myKey)
-      {
-        myMinNode = newNode;
-      }
-      newNode.parent = null;
-      mySize++;
-    }
+        private Node myMinNode; // узел с минимальным корнем
+        private int mySize = 0; // количество узлов
 
-    public int GetMin()
-    {
-      return myMinNode.myKey;
-    }
-
-    public void UnionNodes(Node first, Node second)
-    {
-      if (first != null && second != null)
-      {
-        var oldFirstLeft = first.left;
-        var oldSecondRight = second.right;
-        second.right = first;
-        first.left = second;
-        oldFirstLeft.right = oldSecondRight;
-        oldSecondRight.left = oldFirstLeft;
-      }
-    }
-
-    public void Merge(FibHeap second)
-    {
-      if (second.mySize == 0)
-      {
-        return;
-      }
-      if (mySize == 0)
-      {
-        myMinNode = second.myMinNode;
-        mySize = second.mySize;
-      }
-      else
-      {
-        UnionNodes(myMinNode, second.myMinNode);
-        mySize += second.mySize;
-      }
-      if (myMinNode == null || GetMin() > second.GetMin())
-      {
-        myMinNode = second.myMinNode;
-      }
-    }
-
-    public int Pop()
-    {
-      if (mySize == 0)
-      {
-        throw new InvalidOperationException();
-      }
-      if (myMinNode.right == myMinNode.left)
-      {
-        var key = myMinNode.myKey;
-        myMinNode = null;
-        return key;
-      }
-      var prevMinNode = myMinNode;
-      UnionNodes(myMinNode, myMinNode.child);
-      var oldMinLeft = myMinNode.left;
-      var oldMinRight = myMinNode.right;
-      oldMinLeft.right = oldMinRight;
-      oldMinRight.left = oldMinLeft;
-      myMinNode = myMinNode
-        .right; // пока что перекинем указаиель min на правого сына, а далее consolidate() скорректирует min в процессе выполнения
-      Consolidate();
-      mySize--;
-      return prevMinNode.myKey;
-    }
-
-    private void Consolidate()
-    {
-      var degreeArray = new Node[mySize];
-      degreeArray[myMinNode.Degree] = myMinNode; // создаем массив и инициализируем его min
-      var current = myMinNode.right;
-      while (degreeArray[current.Degree] != current) // пока элементы массива меняются
-      {
-        if (degreeArray[current.Degree] == null) // если ячейка пустая, то положим в нее текущий элемент
+        public void Push(int key)
         {
-          degreeArray[current.Degree] = current;
-          current = current.right;
+            var newNode = new Node(key);
+            if (mySize == 0)
+            {
+                InitMinNode(newNode);
+            }
+            else
+            {
+                PlaceNewNodeOnMinRight(newNode);
+            }
+
+            if (newNode.myKey < myMinNode.myKey)
+            {
+                myMinNode = newNode;
+            }
+            mySize++;
         }
-        else // иначе подвесим к меньшему из текущего корня и того, который лежит в ячейке другой
+
+        private void InitMinNode(Node newNode)
         {
-          var conflict = degreeArray[current.Degree];
-          Node addTo, adding;
-          if (conflict.myKey < current.myKey)
-          {
-            addTo = conflict;
-            adding = current;
-          }
-          else
-          {
-            addTo = current;
-            adding = conflict;
-          }
-          UnionNodes(addTo.child, adding);
-          adding.parent = addTo;
-          addTo.Degree++;
-          current = addTo;
+            myMinNode = newNode;
+            SetNeighbors(newNode, newNode);
         }
-        if (myMinNode.myKey > current.myKey) // обновляем минимум, если нужно
+
+        private void SetNeighbors(Node left, Node right)
         {
-          myMinNode = current;
+            right.left = left;
+            left.right = right;
         }
-      }
+
+        private void PlaceNewNodeOnMinRight(Node newNode)
+        {
+            var prevRightNode = myMinNode.right;
+            SetNeighbors(myMinNode, newNode);
+            SetNeighbors(newNode, prevRightNode);
+        }
+
+        public int GetMin()
+        {
+            return myMinNode.myKey;
+        }
+
+        public void Merge(FibHeap second)
+        {
+            if (second.mySize == 0)
+            {
+                return;
+            }
+            if (mySize == 0)
+            {
+                myMinNode = second.myMinNode;
+                mySize = second.mySize;
+            }
+            else
+            {
+                UnionNodes(myMinNode, second.myMinNode);
+                mySize += second.mySize;
+            }
+            if (myMinNode == null || GetMin() > second.GetMin())
+            {
+                myMinNode = second.myMinNode;
+            }
+        }
+
+
+        private void UnionNodes(Node first, Node second)
+        {
+            if (first != null && second != null)
+            {
+                SetNeighbors(first.left, second.right);
+                SetNeighbors(second, first);
+            }
+        }
+
+        public int Pop()
+        {
+            if (mySize == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            if (myMinNode.right == myMinNode.left)
+            {
+                var key = myMinNode.myKey;
+                myMinNode = null;
+                return key;
+            }
+            var prevMinNode = myMinNode;
+            UnionNodes(myMinNode, myMinNode.child);
+            SetNeighbors(myMinNode.left, myMinNode.right);
+            myMinNode = myMinNode.right; // перекинем указаиель min на правого сына, далее consolidate() скорректирует min
+            Consolidate();
+            mySize--;
+            return prevMinNode.myKey;
+        }
+
+        private void Consolidate()
+        {
+            var degreeArray = new Node[mySize];
+            degreeArray[myMinNode.Degree] = myMinNode; // создаем массив и инициализируем его min
+            var current = myMinNode.right;
+            while (degreeArray[current.Degree] != current) // пока элементы массива меняются
+            {
+                if (degreeArray[current.Degree] == null) // если ячейка пустая, то положим в нее текущий элемент
+                {
+                    degreeArray[current.Degree] = current;
+                    current = current.right;
+                }
+                else // иначе подвесим к меньшему из текущего корня и того, который лежит в ячейке другой
+                {
+                    var conflict = degreeArray[current.Degree];
+                    Node addTo, adding;
+                    if (conflict.myKey < current.myKey)
+                    {
+                        addTo = conflict;
+                        adding = current;
+                    }
+                    else
+                    {
+                        addTo = current;
+                        adding = conflict;
+                    }
+                    UnionNodes(addTo.child, adding);
+                    adding.parent = addTo;
+                    addTo.Degree++;
+                    current = addTo;
+                }
+                if (myMinNode.myKey > current.myKey) // обновляем минимум, если нужно
+                {
+                    myMinNode = current;
+                }
+            }
+        }
     }
 
-    private int GetMaxDegree()
+    internal class Node
     {
-      var maxDegree = myMinNode.Degree;
-      var current = myMinNode.right;
-      while (current != myMinNode)
-      {
-        if (current.Degree > maxDegree)
+        public Node parent = null;
+        public Node child = null;
+        public Node left = null;
+        public Node right = null;
+
+        public int Degree = 0;
+
+        public bool mark;        // метка - был ли удален один из дочерних элементов
+        public int myKey;          // числовое значение узла
+
+        public Node(int key)
         {
-          maxDegree = current.Degree;
+            myKey = key;
         }
-        current = current.right;
-      }
-      return maxDegree;
+
     }
-  }
-
-  internal class Node
-  {
-    public Node parent;
-    public Node child;
-    public Node left;
-    public Node right;
-
-    public int Degree = 0;
-
-    public bool mark;        // метка - был ли удален один из дочерних элементов
-    public int myKey;          // числовое значение узла
-
-    public Node(int key)
-    {
-      myKey = key;
-    }
-  }
 }
